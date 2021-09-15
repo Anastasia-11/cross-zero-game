735f9ef9f39adf194bb6116061ad2035f19d650a
Anastasia-11 <55688203+Anastasia-11@users.noreply.github.com>
2021-09-15 12:13:24 +0300

    Add files via upload

---
+++ Cross-zero/Cross-zero/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace Cross_zero
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.labelDrawnGame = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.nudSize = new System.Windows.Forms.NumericUpDown();
+            this.labelScoreUser2 = new System.Windows.Forms.Label();
+            this.labelScoreUser1 = new System.Windows.Forms.Label();
+            this.labelUser2 = new System.Windows.Forms.Label();
+            this.labelUser1 = new System.Windows.Forms.Label();
+            this.labelStatistics = new System.Windows.Forms.Label();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudSize)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.FixedPanel = System.Windows.Forms.FixedPanel.Panel2;
+            this.splitContainer1.IsSplitterFixed = true;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.labelDrawnGame);
+            this.splitContainer1.Panel1.Controls.Add(this.pictureBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.nudSize);
+            this.splitContainer1.Panel2.Controls.Add(this.labelScoreUser2);
+            this.splitContainer1.Panel2.Controls.Add(this.labelScoreUser1);
+            this.splitContainer1.Panel2.Controls.Add(this.labelUser2);
+            this.splitContainer1.Panel2.Controls.Add(this.labelUser1);
+            this.splitContainer1.Panel2.Controls.Add(this.labelStatistics);
+            this.splitContainer1.Panel2.Controls.Add(this.buttonStart);
+            this.splitContainer1.Size = new System.Drawing.Size(809, 556);
+            this.splitContainer1.SplitterDistance = 656;
+            this.splitContainer1.TabIndex = 1;
+            // 
+            // labelDrawnGame
+            // 
+            this.labelDrawnGame.BackColor = System.Drawing.Color.Transparent;
+            this.labelDrawnGame.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelDrawnGame.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.labelDrawnGame.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.labelDrawnGame.Font = new System.Drawing.Font("ROG Fonts", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDrawnGame.Location = new System.Drawing.Point(0, 0);
+            this.labelDrawnGame.Name = "labelDrawnGame";
+            this.labelDrawnGame.Size = new System.Drawing.Size(656, 556);
+            this.labelDrawnGame.TabIndex = 1;
+            this.labelDrawnGame.Text = "DRAWN GAME";
+            this.labelDrawnGame.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.labelDrawnGame.Visible = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(656, 556);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            // 
+            // nudSize
+            // 
+            this.nudSize.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.nudSize.Font = new System.Drawing.Font("ROG Fonts", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.nudSize.Location = new System.Drawing.Point(10, 127);
+            this.nudSize.Maximum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.nudSize.Minimum = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            this.nudSize.Name = "nudSize";
+            this.nudSize.Size = new System.Drawing.Size(120, 28);
+            this.nudSize.TabIndex = 6;
+            this.nudSize.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.nudSize.Value = new decimal(new int[] {
+            3,
+            0,
+            0,
+            0});
+            // 
+            // labelScoreUser2
+            // 
+            this.labelScoreUser2.AutoSize = true;
+            this.labelScoreUser2.Font = new System.Drawing.Font("ROG Fonts", 10F);
+            this.labelScoreUser2.Location = new System.Drawing.Point(6, 382);
+            this.labelScoreUser2.Name = "labelScoreUser2";
+            this.labelScoreUser2.Size = new System.Drawing.Size(24, 20);
+            this.labelScoreUser2.TabIndex = 5;
+            this.labelScoreUser2.Text = "0";
+            // 
+            // labelScoreUser1
+            // 
+            this.labelScoreUser1.AutoSize = true;
+            this.labelScoreUser1.Font = new System.Drawing.Font("ROG Fonts", 10F);
+            this.labelScoreUser1.Location = new System.Drawing.Point(12, 308);
+            this.labelScoreUser1.Name = "labelScoreUser1";
+            this.labelScoreUser1.Size = new System.Drawing.Size(24, 20);
+            this.labelScoreUser1.TabIndex = 4;
+            this.labelScoreUser1.Text = "0";
+            // 
+            // labelUser2
+            // 
+            this.labelUser2.AutoSize = true;
+            this.labelUser2.Font = new System.Drawing.Font("ROG Fonts", 10F);
+            this.labelUser2.Location = new System.Drawing.Point(12, 345);
+            this.labelUser2.Name = "labelUser2";
+            this.labelUser2.Size = new System.Drawing.Size(82, 20);
+            this.labelUser2.TabIndex = 3;
+            this.labelUser2.Text = "User2";
+            // 
+            // labelUser1
+            // 
+            this.labelUser1.AutoSize = true;
+            this.labelUser1.Font = new System.Drawing.Font("ROG Fonts", 10F);
+            this.labelUser1.Location = new System.Drawing.Point(12, 267);
+            this.labelUser1.Name = "labelUser1";
+            this.labelUser1.Size = new System.Drawing.Size(76, 20);
+            this.labelUser1.TabIndex = 2;
+            this.labelUser1.Text = "User1";
+            // 
+            // labelStatistics
+            // 
+            this.labelStatistics.AutoSize = true;
+            this.labelStatistics.Font = new System.Drawing.Font("ROG Fonts", 10.2F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelStatistics.Location = new System.Drawing.Point(6, 217);
+            this.labelStatistics.Name = "labelStatistics";
+            this.labelStatistics.Size = new System.Drawing.Size(140, 20);
+            this.labelStatistics.TabIndex = 1;
+            this.labelStatistics.Text = "Statistics";
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.buttonStart.Font = new System.Drawing.Font("ROG Fonts", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonStart.Location = new System.Drawing.Point(2, 61);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(144, 38);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = false;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(809, 556);
+            this.Controls.Add(this.splitContainer1);
+            this.MinimumSize = new System.Drawing.Size(300, 300);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            this.splitContainer1.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.nudSize)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label labelScoreUser2;
+        private System.Windows.Forms.Label labelScoreUser1;
+        private System.Windows.Forms.Label labelUser2;
+        private System.Windows.Forms.Label labelUser1;
+        private System.Windows.Forms.Label labelStatistics;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.NumericUpDown nudSize;
+        private System.Windows.Forms.Label labelDrawnGame;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Cross-zero/Cross-zero/Form1.cs
@@ -0,0 +1,383 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cross_zero
+{
+    public partial class Form1 : Form
+    {
+        // Field.
+        Graphics graphics;
+        private List<KeyValuePair<int, int>> field;
+        private int delta;
+        private int indentW;
+        private int indentH;
+        private int cellAmount;
+        private int cellSize;
+
+        // Elements.
+        Symbol symbol;
+        private bool[,] crosses;
+        private bool[,] zeros;
+        private int crossesScore = 0;
+        private int zerosScore = 0;
+        private bool win = false;
+
+        private enum Symbol
+        {
+            Cross,
+            Zero
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            labelDrawnGame.Parent = pictureBox1; // For label transparency.
+            
+        }
+
+        //private void Form1_Load(object sender, EventArgs e)
+        //{
+        //    buttonStart_Click(sender, e);
+        //}
+
+        private void timer1_Tick(object sender, EventArgs e)
+        { 
+            DrawLines();
+            DrawCrosses();
+            DrawZeros();
+            pictureBox1.Invalidate();
+            CheckFieldFullness();
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            if (labelDrawnGame.Visible == true)
+                labelDrawnGame.Visible = false;
+
+            win = false;
+            symbol = Symbol.Cross;
+            cellAmount = (int)nudSize.Value;
+            nudSize.Enabled = false;
+
+            field = new List<KeyValuePair<int, int>>();
+            crosses = new bool[cellAmount, cellAmount];
+            zeros = new bool[cellAmount, cellAmount];
+            timer1.Start();
+        }
+
+        private void DrawLines()
+        {
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            graphics = Graphics.FromImage(pictureBox1.Image);
+            graphics.Clear(Color.Empty);
+
+            delta = Math.Min(pictureBox1.Width, pictureBox1.Height);
+            indentW = (pictureBox1.Width - delta) / 2;
+            indentH = (pictureBox1.Height - delta) / 2;
+            cellSize = delta / cellAmount;
+
+            // Vertical lines.
+            for (int x = indentW, i = 0; i < cellAmount - 1; i++, x += cellSize)
+            {
+                graphics.DrawLine(Pens.Black, x + cellSize, indentH, x + cellSize, pictureBox1.Height - indentH);
+            }
+            // Horizontal lines.
+            for (int y = indentH, i = 0; i < cellAmount - 1; i++, y += cellSize)
+            {
+                graphics.DrawLine(Pens.Black, indentW, y + cellSize, pictureBox1.Width - indentW, y + cellSize);
+            }       
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (win)
+                return;
+            int cellX, cellY;
+            int cursorX = e.Location.X;
+            int cursorY = e.Location.Y;
+            CalculateCellCoordinates(out cellX, out cellY, cursorX, cursorY);
+
+            if (ClickInField(cellX, cellY) && CellIsEmpty(cellX, cellY))
+            {
+                AddSymbolToField(cellX, cellY);
+                switch (symbol)
+                {
+                    case Symbol.Cross:
+                        {
+                            symbol = Symbol.Zero;
+                            if (IsWinCombination(cellX, cellY, crosses))
+                            {
+                                labelScoreUser1.Text = $"{++crossesScore}";
+                                win = true;
+                                //timer1.Stop();
+                                nudSize.Enabled = true;
+                                return;
+                            }
+                            break;
+                        }
+                    case Symbol.Zero:
+                        {
+                            symbol = Symbol.Cross;
+                            if (IsWinCombination(cellX, cellY, zeros))
+                            {
+                                labelScoreUser2.Text = $"{++zerosScore}";
+                                win = true;
+                                //timer1.Stop();
+                                nudSize.Enabled = true;
+                                return;
+                            }
+                            break;
+                        }
+                }
+            }
+        }
+
+        private bool ClickInField(int cell_x, int cell_y)
+        {
+            return (cell_x > 0 && cell_x <= cellAmount
+                && cell_y > 0 && cell_y <= cellAmount);
+        }
+
+        private bool CellIsEmpty(int cell_x, int cell_y)
+        {
+            foreach (var cell in field)
+            {
+                if (cell.Key == cell_x && cell.Value == cell_y)
+                    return false;
+            }
+            return true;
+        }
+
+        private void CalculateCellCoordinates(out int cell_x, out int cell_y, int cursor_x, int cursor_y)
+        {
+            int x = indentW, y = indentH;
+            cell_x = 0;
+            cell_y = 0;
+            while (x <= cursor_x)
+            {
+                x += cellSize;
+                cell_x++;
+            }
+            while (y <= cursor_y)
+            {
+                y += cellSize;
+                cell_y++;
+            }
+        }
+
+        private void AddSymbolToField(int cell_x, int cell_y)
+        {
+            field.Add(new KeyValuePair<int, int>(cell_x, cell_y));
+            switch (symbol)
+            {
+                case Symbol.Cross:
+                    {
+                        crosses[cell_x - 1, cell_y - 1] = true;
+                        break;
+                    }
+                case Symbol.Zero:
+                    {
+                        zeros[cell_x - 1, cell_y - 1] = true;
+                        break;
+                    }
+            }
+        }
+
+        private void DrawCrosses()
+        {
+            int leftX, rightX, bottomY, upY;
+            for(int x = 0; x < cellAmount; x++)
+            {
+                for (int y = 0; y < cellAmount; y++)
+                {
+                    if(crosses[x, y])
+                    {
+                        leftX = indentW + x * cellSize;
+                        rightX = indentW + (x + 1) * cellSize;
+                        bottomY = indentH + (y + 1) * cellSize;
+                        upY = indentH + y * cellSize;
+                        graphics.DrawLine(Pens.Black, leftX, upY, rightX, bottomY);
+                        graphics.DrawLine(Pens.Black, leftX, bottomY, rightX, upY);
+                    }
+                }
+            }
+        }
+
+        private void DrawZeros()
+        {
+            int centerX, centerY;
+            for (int x = 0; x < cellAmount; x++)
+            {
+                for (int y = 0; y < cellAmount; y++)
+                {
+                    if (zeros[x, y])
+                    {
+                        centerX = indentW + x * cellSize;
+                        centerY= indentH + y * cellSize;
+                        graphics.DrawEllipse(Pens.Black, centerX, centerY, cellSize, cellSize);
+                    }
+                }
+            }
+        }
+
+        private bool IsWinCombination(int x, int y, bool[,] array)
+        {
+            // Vertical check.
+            for (int i = 0; i < cellAmount; i++)
+            {
+                if (i == cellAmount - 1 && array[i, y - 1])
+                {
+                    DrawWinCombination(1, y, cellAmount, y);
+                    return true;
+                }
+                if (!array[i, y - 1])
+                    break;
+            }
+            // Horizontal check.
+            for (int j = 0; j < cellAmount; j++)
+            {
+                if (j == cellAmount - 1 && array[x - 1, j])
+                {
+                    DrawWinCombination(x, 1, x, cellAmount);
+                    return true;
+                }
+                if (!array[x - 1, j])
+                    break;
+            }
+            // Check from left top to right botom point
+            for (int i = 0, j = 0; i < cellAmount; i++, j++)
+            {
+                if (i == cellAmount - 1 && array[i, j])
+                {
+                    DrawWinCombination(1, 1, cellAmount, cellAmount);
+                    return true;
+                }
+                if (!array[i, j])
+                    break;
+            }
+            // Check from left bottom to right top point
+            for (int i = 0, j = cellAmount - 1; i < cellAmount; i++, j--)
+            {
+                if (i == cellAmount - 1 && array[i, j])
+                {
+                    DrawWinCombination(1, cellAmount, cellAmount, 1);
+                    return true;
+                }
+                if (!array[i, j])
+                    break;
+            }
+            return false;
+        }
+
+        private void CheckFieldFullness()
+        {
+            for(int i = 0; i < cellAmount; i++)
+            {
+                for(int j = 0; j < cellAmount; j++)
+                {
+                    if (!crosses[i, j] && !zeros[i, j])
+                        return;
+                }
+            }
+            labelDrawnGame.Visible = true;
+            nudSize.Enabled = true;
+            //timer1.Stop();
+        }
+
+        private void DrawWinCombination(int x1, int y1, int x2, int y2)
+        {
+            int deltaX = (x1 - x2 + 1) / cellAmount;
+            int deltaY = (y1 - y2 + 1) / cellAmount;
+            for (int i = 0, x = x1, y = y1; i < cellAmount; i++, x += deltaX, y +=deltaY)
+            {
+
+            }
+        }
+
+        //private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        //{
+        //    if (win)
+        //        return;
+        //    int cellX, cellY;
+        //    int cursorX = e.Location.X;
+        //    int cursorY = e.Location.Y;
+        //    CalculateCellCoordinates(out cellX, out cellY, cursorX, cursorY);
+
+        //    if (CellIsEmpty(cellX, cellY))
+        //    {
+        //        switch (symbol)
+        //        {
+        //            case Symbol.Cross:
+        //                {
+        //                    DrawCrossUnderMouse(cellX, cellY);
+        //                    break;
+        //                }
+        //            case Symbol.Zero:
+        //                {
+        //                    DrawZeroUnderMouse(cellX, cellY);
+        //                    break;
+        //                }
+        //        }
+        //    }
+        //}
+
+        //private void DrawCrossUnderMouse(int cell_x, int cell_y)
+        //{
+        //    int leftX = indentW + (cell_x - 1) * cellSize;
+        //    int rightX = indentW + cell_x * cellSize;
+        //    int bottomY = indentH + cell_y * cellSize;
+        //    int upY = indentH + (cell_y - 1) * cellSize;
+            
+        //    graphics.DrawLine(Pens.Gray, leftX, upY, rightX, bottomY);
+        //    graphics.DrawLine(Pens.Gray, leftX, bottomY, rightX, upY);
+                   
+        //    pictureBox1.Refresh();
+        //}
+
+        //private void DrawZeroUnderMouse(int cell_x, int cell_y)
+        //{
+        //    int centerX = indentW + (cell_x - 1) * cellSize;
+        //    int centerY = indentH + (cell_y - 1) * cellSize;
+            
+        //    graphics.DrawEllipse(Pens.Black, centerX, centerY, cellSize, cellSize);
+                   
+        //    pictureBox1.Refresh();
+        //}
+
+        //private void pictureBox1_MouseHover(object sender, EventArgs e)
+        //{
+        //    if (win)
+        //        return;
+        //    MouseEventArgs mouseEventArgs;
+            
+        //    int cellX, cellY;
+        //    int cursorX = mouseEventArgs.Location.X; ;
+        //    int cursorY = e.Location.Y;
+        //    CalculateCellCoordinates(out cellX, out cellY, cursorX, cursorY);
+
+        //    if (CellIsEmpty(cellX, cellY))
+        //    {
+        //        switch (symbol)
+        //        {
+        //            case Symbol.Cross:
+        //                {
+        //                    DrawCrossUnderMouse(cellX, cellY);
+        //                    break;
+        //                }
+        //            case Symbol.Zero:
+        //                {
+        //                    DrawZeroUnderMouse(cellX, cellY);
+        //                    break;
+        //                }
+        //        }
+        //    }
+        //}
+    }
+}
